ec0b49aba46347796022412a8ffda94f799c74b9
Mihir Kumar Singh <mihirkumar02@gmail.com>
2021-04-01 13:56:41 +0530

    first deployment config

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    DOMAIN: process.env.DOMAIN,
+    EMAIL: process.env.EMAIL,
+    PASSWORD: process.env.PASSWORD
+}
